7a5957b8f095ae7badbc6d7272e3f3d8b8993a9e
Jan Komiš <48412748+JanKomis@users.noreply.github.com>
2024-05-20 09:03:28 +0200

    base setup auth js

---
+++ auth.config.ts
@@ -0,0 +1,21 @@
+import type { NextAuthConfig } from "next-auth";
+import { Provider } from "next-auth/providers";
+import Google from "next-auth/providers/google";
+
+const providers: Provider[] = [Google];
+
+export const providerMap = providers.map((provider) => {
+  if (typeof provider === "function") {
+    const providerData = provider();
+    return { id: providerData.id, name: providerData.name };
+  } else {
+    return { id: provider.id, name: provider.name };
+  }
+});
+
+export default {
+  providers,
+  pages: {
+    signIn: "/signin",
+  },
+} satisfies NextAuthConfig;
